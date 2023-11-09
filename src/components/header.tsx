5384d5d6cb14352611984b1dee46924db40e538a
Francis <francisbernal14@gmail.com>
2023-11-09 20:05:37 +0100

    adding routes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 import NavLinks from "./nav-links";
 import ToggleButton from "./toggleButton";
 
@@ -6,11 +7,13 @@ export default function Header() {
 
     return (
         <header className="bg-dark flex flex-row justify-around items-center h-12">
-            <img
-                src="/assets/logo-dgrana-tapas-blanco.png"
-                alt="logotipo del restaurante"
-                className="w-auto h-8"
-            />
+            <Link href="/">
+                <img
+                    src="/assets/logo-dgrana-tapas-blanco.png"
+                    alt="logotipo del restaurante"
+                    className="w-auto h-8"
+                />
+            </Link>
             <div className="hidden md:block">
                 <NavLinks />
             </div>
