185b3591a2d96178f5f03329391322bbf48e3d80
Francis <francisbernal14@gmail.com>
2023-11-19 22:46:48 +0100

    add product metadata

--- src/app/menu/[category]/[product]/page.tsx
+++ src/app/menu/[category]/[product]/page.tsx
@@ -2,6 +2,19 @@ import FullCardPlate from "@/components/FullCardPlate"
 import { getMenuByCategory } from "@/lib/services"
 import { urlDasher } from "@/lib/util"
 
+export function generateMetadata({ params }: { params: { category: string, product: string } }) {
+    const { category, product: name } = params
+    const product = getMenuByCategory(category).find(plate => encodeURIComponent(urlDasher(plate.name)) == name)
+
+    return {
+        title: product?.name,
+        description: product?.description,
+        openGraph: {
+            images: product?.img
+        }
+    }
+}
+
 export default function ProductPage({ params }: { params: { category: string, product: string } }) {
     const { category, product: name } = params
     const product = getMenuByCategory(category).find(plate => encodeURIComponent(urlDasher(plate.name)) == name)
