7db5be24ab0ceee15a9268547f4ab5bd308b1955
Francis <francisbernal14@gmail.com>
2023-11-24 04:11:06 +0100

    testing middleware language

---
+++ src/middleware.js
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server'
+import Negotiator from "negotiator";
+import { match } from "@formatjs/intl-localematcher";
+
+let locales = ['es', 'en']
+let defaultLocale = 'es'
+
+export const getLocale = (request) => {
+    let languages = new Negotiator(request).languages()
+
+    if (languages.length === 1 && languages[0] === '*') {
+        return defaultLocale
+    }
+
+    return match(languages, locales, defaultLocale)
+}
+
+export default function middleware(request) {
+    // Check if there is any supported locale in the pathname
+    const { pathname } = request.nextUrl
+    const pathnameHasLocale = locales.some(
+        (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    )
+
+    if (pathnameHasLocale) return
+
+    // Redirect if there is no locale
+    const locale = getLocale(request)
+
+    if (locale === 'es') return 
+
+    request.nextUrl.pathname = `/${locale}${pathname}`
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return Response.redirect(request.nextUrl)
+}
+
+export const config = {
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+}
