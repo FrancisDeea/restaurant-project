1412f150c01390b4c7918653688406f2e0d6c7dd
Francis <francisbernal14@gmail.com>
2023-11-30 21:00:01 +0100

    create drinks page

---
+++ src/app/[lang]/menu/bebidas/page.tsx
@@ -0,0 +1,316 @@
+export default function Bebidas() {
+
+    return (
+        <>
+            <section className="bg-black/60 max-w-[800px] w-full lg:col-span-full lg:place-self-center lg:text-xl">
+                <div className="flex flex-col gap-2 p-2 w-full lg:gap-8">
+
+                    <header>
+                        <h1>Bebidas</h1>
+                    </header>
+
+                    <div className="w-full">
+                        <h2 className="text-left text-amber-200">Refrescos</h2>
+                                                <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Coca-Cola original / zero / zero-zero
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.70
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Fanta naranja / limón
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.70
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Nestea limón / Maracuya
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.70
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Tónica
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.70
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Tinto de verano &quot;La Casera&quot;
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.70
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Tinto de verano casero (limón o blanca)
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.70
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Maceta grande de tinto verano casero
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.50
+                            </p>
+                        </article>
+                    </div>
+
+                    <div>
+                        <h2 className="text-left text-amber-200">Vino Tinto</h2>
+                        <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Rioja
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.00
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Ribera
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.50
+                            </p>
+                        </article>
+                    </div>
+
+                    <div>
+                        <h2 className="text-left text-amber-200">Vino Blanco</h2>
+                                                <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Verdejo
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.00
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Frizzante
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.00
+                            </p>
+                        </article>
+                    </div>
+
+                    <div>
+                        <h2 className="text-left text-amber-200">Cerveza Alhambra (Grifo)</h2>
+                                                <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Caña
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.20
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Copa
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.30
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Pinta/Maceta
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.80
+                            </p>
+                        </article>
+                    </div>
+
+                    <div>
+                        <h2 className="text-left text-amber-200">Tercios de Cerveza</h2>
+                                                <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Alhambra especial
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.80
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Alhambra 1925
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.50
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Alhambra Reserva Roja
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.50
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Alhambra IPA
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                3.50
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Radler (con limón)
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.80
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                San Miguel 0.0
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.80
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                San Miguel sin gluten
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.80
+                            </p>
+                        </article>
+                    </div>
+
+                    <div>
+                        <h2 className="text-left text-amber-200">Copas/Combinados</h2>
+                                                <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Normal
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                5.50
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Premium
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                6.50
+                            </p>
+                        </article>
+                    </div>
+
+                    <div>
+                        <h2 className="text-left text-amber-200">Café y té</h2>
+                                                <hr className="border-none h-[1px] bg-amber-100 mb-2" />
+
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Solo, cortado, con leche, manchada
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                1.50
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Bombón o carajillo
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                2.00
+                            </p>
+                        </article>
+
+                        <article className="w-full">
+                            <p className="w-[80%] inline-block">
+                                Té o infusion
+                            </p>
+
+                            <p className="w-[20%] inline-block text-right">
+                                1.50
+                            </p>
+                        </article>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
